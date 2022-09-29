c5fbc607b1e48ec0b553c5dbda47a136bf0786fc
Daniel Rotstein Gonçalves <rotsteindaniel@gmail.com>
2022-09-29 08:05:31 -0300

    header-layout-routes-theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,16 @@
+import { ThemeProvider } from 'styled-components'
+import { Router } from './Router'
+import { BrowserRouter } from 'react-router-dom'
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/themes/default'
+
 export function App() {
-  return <h1>Hello</h1>;
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+      <GlobalStyle />
+    </ThemeProvider>
+  )
 }
